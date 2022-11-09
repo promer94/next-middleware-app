48ee6e2fc3454219abf15a54ab1b0fb8f76d6997
Yixuan Xu <yixuanxu94@outlook.com>
2022-11-09 21:50:36 +0800

    update

--- app/middleware.js
+++ app/middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
  * @param {import('next/server').NextRequest} request
  */
 export async function middleware() {
-	const res = NextResponse.next({ request: { headers: { "x-from-middleware": "hello-from-middleware" } } });
+	const res = NextResponse.next();
 	res.headers.set("Set-Cookie", "foo=bar; Path=/; HttpOnly");
 	return res;
 }
